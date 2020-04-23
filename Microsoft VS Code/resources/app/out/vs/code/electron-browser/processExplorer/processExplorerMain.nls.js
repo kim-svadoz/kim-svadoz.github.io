e6a01a5c8de33f58f75bdba6663ceb1ba4b9c405
kim-svaodz <dhkdghehfdl@gmail.com>
2020-04-23 15:36:48 +0900

    portfolio

---
+++ Microsoft VS Code/resources/app/out/vs/code/electron-browser/processExplorer/processExplorerMain.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/code/electron-browser/processExplorer/processExplorerMain.nls",{"vs/code/electron-browser/processExplorer/processExplorerMain":["CPU %","Memory (MB)","pid","Name","Kill Process","Force Kill Process","Copy","Copy All","Debug"]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/ff915844119ce9485abfe8aa9076ec76b5300ddd/core/vs/code/electron-browser/processExplorer/processExplorerMain.nls.js.map
